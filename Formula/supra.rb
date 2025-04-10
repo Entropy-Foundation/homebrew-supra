36af7c2f476e14b54117d3c143f3c32bfea6dcb0
Rakshith Ravi <rakshith.ravi@gmx.com>
2025-04-11 00:08:52 +0530

    Try fix error

--- Formula/supra.rb
+++ Formula/supra.rb
@@ -1,13 +1,11 @@
 class Supra < Formula
-    version "v9.0.3"
-
-    name "supra"
     desc "Moonshot SMR integration with Move Lang support"
     homepage "https://supraoracles.com"
+    version "v9.0.3"
     url "https://pub-aa772efaa01a41deb1679acfca2d28b3.r2.dev/releases/supra_node_#{version}-stripped.zip"
 
     def install
-        bin.install 'build_output/stripped/supra'
-        bin.install 'build_output/stripped/rpc_node'
+        bin.install "build_output/stripped/supra"
+        bin.install "build_output/stripped/rpc_node"
     end
 end
