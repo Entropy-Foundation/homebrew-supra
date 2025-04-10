0ef4caa51517d0a0f601fb2046efe10829039cce
Rakshith Ravi <rakshith.ravi@gmx.com>
2025-04-11 00:24:11 +0530

    Added linux only

--- Formula/supra.rb
+++ Formula/supra.rb
@@ -2,7 +2,13 @@ class Supra < Formula
     desc "Moonshot SMR integration with Move Lang support"
     homepage "https://supraoracles.com"
     version "v9.0.3"
-    url "https://pub-aa772efaa01a41deb1679acfca2d28b3.r2.dev/releases/supra_node_#{version}-stripped.zip"
+    on_linux do
+        on_intel do
+            url "https://pub-aa772efaa01a41deb1679acfca2d28b3.r2.dev/releases/supra_node_#{version}-stripped.zip"
+        end
+        # on_arm do doesn't exist for now
+    end
+    # on_macos do doesn't exist for now
 
     def install
         bin.install "stripped/supra"
